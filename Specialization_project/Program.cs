44b8067310b5ef3b1ca5d33db3d4b8b30bf3c560
TatianaKolo <manila-nick@yandex.ru>
2022-12-14 12:00:47 +0300

    Specialization_project is finaly ready

--- Specialization_project/Program.cs
+++ Specialization_project/Program.cs
@@ -7,22 +7,6 @@
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → []
-
-string[] stringArray = new string[8] { "Rus", "UK", "Kazan", "Argentina", "Belarus", "China", "Guatemala", "USA"};
-string[] minArray = new string[stringArray.Length];
-int letter_counter = 0;
-int size = 3;
-for (int i = 0; i<stringArray.Length; i++)
-{
-    if (stringArray[i].Length <=size)
-    {
-        minArray[letter_counter]=stringArray[i];  
-        letter_counter++;   
-    }
-}
-Console.Write($"[{String.Join(" ", stringArray)}] ->");
-Console.Write($"[{String.Join( " ", minArray)}]");
-
 */
 
 void InputArray(string[] array)
